812dafa40cfb34be81c2ab2fb861ebe1e09d10eb
Ni Chen (git) <chenni@snu.ac.kr>
2019-08-02 23:24:42 +0900

    batch added

--- data/hair_param.m
+++ data/hair_param.m
@@ -15,8 +15,9 @@ lambda = 532e-9;    % wavelength of illumination beam
 pps = 3.45e-6;      % pixel pitch of CCD camera
 
 % z = [132 -4 3]*1e-3;
-% z = [135 0 8]*1e-3;
-z = [85 -50 -42]*1e-3;
+% z = [0 8 135]*1e-3;
+z = [-16 -8 0 8 16 24 32 40 48 56 64 72 80 88 96 104 120 128 135 143]*1e-3;
+% z = [85 -50 -42]*1e-3;
 % z = [132 133 134 135 136]*1e-3;
 Nz = length(z);
 
@@ -29,7 +30,7 @@ tau_psi = 0.2;
 %% ========================================== Resize =================================================
 load('hair.mat');
 
-[holo, pps] = holoResize(holo, pps, 128, 20);
+[holo, pps] = holoResize(holo, pps, 512, 5);
 [Ny, Nx] = size(holo);
 
 
